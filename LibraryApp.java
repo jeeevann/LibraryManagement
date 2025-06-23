f041bbc5f5f9fcf49c560380ab8f7c8ccb9c7acc
Jeevan Bhandare <125866578+jeeevann@users.noreply.github.com>
2025-06-23 11:07:15 +0530

    Add files via upload

--- LibraryApp.java
+++ LibraryApp.java
@@ -24,19 +24,26 @@ public class LibraryApp {
                     String title = sc.nextLine();
                     System.out.print("Author: ");
                     String author = sc.nextLine();
-                    lib.addBook(new Book(id, title, author));
+                    System.out.print("Stock: ");
+                    int stock = sc.nextInt();
+                    sc.nextLine(); // consume newline
+                    lib.addBook(new Book(id, title, author, stock));
                 }
                 case 2 -> {
                     System.out.print("User ID: ");
                     String id = sc.nextLine();
                     System.out.print("Name: ");
                     String name = sc.nextLine();
-                    lib.registerUser(new User(id, name));
+                    System.out.print("Password: ");
+                    String password = sc.nextLine();
+                    lib.registerUser(new User(id, name, password, "user"));
                 }
                 case 3 -> {
                     System.out.print("Enter Book ID to issue: ");
                     String bookId = sc.nextLine();
-                    lib.issueBook(bookId);
+                    System.out.print("Enter User ID: ");
+                    String userId = sc.nextLine();
+                    lib.issueBook(bookId, userId);
                 }
                 case 4 -> {
                     System.out.print("Enter Book ID to return: ");
